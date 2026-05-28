068e6320ec93b650509ccc45bbe4b1f9000dd890
Tulsi Sharma <tulsisharma7069@gmail.com>
2026-05-29 00:24:42 +0530

    Used Module pyttsx3

---
+++ ModuleUse.py
@@ -0,0 +1,7 @@
+#Used for speaing text
+
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.say("I will speak this text")
+engine.runAndWait()
--- SumOfN.py
+++ SumOfN.py
@@ -1,3 +1,5 @@
-n = int(input())
-tot = 0
-for (i in range(1,n+1))
+# n = int(input())
+# tot = 0
+# for (i in range(1,n+1))
+import flask 
+print(flask)
