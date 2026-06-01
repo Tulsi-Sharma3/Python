33cb3f9219e0ef008c1fcd53c1b24c06cb2fcd59
Tulsi Sharma <tulsisharma7069@gmail.com>
2026-06-01 23:58:35 +0530

    find function

--- list.py
+++ list.py
@@ -1,3 +1,8 @@
-li = [1,5,8,4,5]
-a = li.sort()
-print(a)
+# li = [1,5,8,4]
+# a = li.insert(2,10)
+# print("hello")
+# print(a)
+# print("hii" ,li.append(9))
+
+name = "Harry is a good  boy"
+print(name.find("  "))
