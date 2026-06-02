8e09a77870b09b98c3152a1dde6681d7011c471d
Tulsi Sharma <tulsisharma7069@gmail.com>
2026-06-03 00:06:36 +0530

    printing from 2 to 10 using range

--- range.py
+++ range.py
@@ -1,9 +1,18 @@
 # range(start,stop,step)
-print(range(5))
-seq = range(5)
-print(seq[0])
-print(seq[1])
-print(seq[2])
-print(seq[3])
-print(seq[4])
-print(seq[5])
+# print(range(5))  # range(0,5)
+
+# seq = range(5)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+# print(seq[5])  #error
+
+#Another way
+# seq = range(5)
+# for i in seq:
+#     print(i)
+
+for i in range(2,10):
+    print(i)
