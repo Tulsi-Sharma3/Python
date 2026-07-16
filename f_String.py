6d3c32f85a09e67b13e633cd1cc84e8aed2c2276
Tulsi Sharma <tulsisharma7069@gmail.com>
2026-07-16 15:37:41 +0530

    Try to print f string as it is like literals

--- f_String.py
+++ f_String.py
@@ -12,16 +12,17 @@ country = "India"
 
 print(letter.format(country,name))
 
-# # 3.
-# name = input("enter name : ")
-# country = input("enter coutry : ")
-# print(f" hey i am {name} and my country is {country}")
+# 3.
+name = input("enter name : ")
+country = input("enter coutry : ")
+print(f" hey i am {name} and my country is {country}")
+print(f" hey i am {{name}} and my country is {{country}}")
 
-# # 4.
-# dec = float(input("enter decimal values"))
-# print(f"decimal value will be : {dec}")
+# 4.
+dec = float(input("enter decimal values"))
+print(f"decimal value will be : {dec}")
 
-# # 5.(upto 2 decimal places)
+# 5.(upto 2 decimal places)
 price = 33.333
 txt = f"for my val {price: .2f}"
 print(txt)
